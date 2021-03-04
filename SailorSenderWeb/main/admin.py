adedea887dd6a24e981408709f9d755c6c404a21
Artemka <avanglion59@gmail.com>
2021-03-04 14:12:39 +0200

    django add models v0.5

--- SailorSenderWeb/main/admin.py
+++ SailorSenderWeb/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Theame
 
+admin.site.register(Theame)
 # Register your models here.
--- SailorSenderWeb/main/models.py
+++ SailorSenderWeb/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Theame(models.Model):
+    theame_text = models.CharField(max_length=20, help_text="Text of the theame")
+
+    theame_user = models.CharField(max_length = 16, help_text = "Username")
+
+    class Meta:
+        ordering = ["-theame_text"]
+
+    # def get_absolute_url(self):
+    #      return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        return self.theame_text
